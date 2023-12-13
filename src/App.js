731877a6e55b24dc3c63660d0497790a68162e2b
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-12-13 12:04:13 +0530

    header task done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Fragment} from 'react';
+import Header from './components/Layout/Header';
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header/>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,18 @@
+// CartIcon.js
+
+import React from 'react';
+
+const CartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      {/* Add your path elements here */}
+      <path d="M3 1l10 8-1 3h6v5h-14zm2 15a1 1 0 100 2 1 1 0 000-2zm12 0a1 1 0 100 2 1 1 0 000-2z" />
+    </svg>
+  );
+};
+
+export default CartIcon;
